d20beb8c997bfb44ec9bb0b7bc9e75e1aafd3fb5
Sarah Hayat <saraahyt@gmail.com>
2021-11-29 14:53:35 +0100

    Premier pas

--- PremierPas/index.js
+++ PremierPas/index.js
@@ -1,23 +1,81 @@
 const fs = require('fs');
 const csv = require('csv-parser')
 const users = [];
-fs.createReadStream("text.csv")
-    .pipe(csv())
-    .on('data', function (row) {
-        const username = generateUsername(row.Firstname, row.Surname);
+const stats = [];
+const fileName = 'text.csv'
+let average_feet = [];
+let average_height = [];
 
-        const user = {
-            username,
-            firstname: row.Firstname,
-            surname: row.Surname,
-        }
-        users.push(user)
-    })
-    .on('end', function () {
-        console.table(users)
-        // TODO: SAVE users data to another file
-    })
+function main() {
+    if (verifyFileExist(fileName) && verifyIsCsvFile(fileName)) {
+
+        console.log("je suis dans le fichier ", fileName)
+        fs.createReadStream(fileName)
+            .pipe(csv())
+            .on('data', function (row) {
+                const username = generateUsername(row.Firstname, row.Surname);
+                addToFeetTab(row.Feet);
+                addToHeightTab(row.Height);
+
+                const user = {
+                    username,
+                    firstname: row.Firstname,
+                    surname: row.Surname,
+                    feet: row.Feet,
+                    height: row.Height,
+
+                }
+                users.push(user)
+            })
+            .on('error', function (err) {
+                console.log("ERROR = ", err)
+                // do something with `err`
+            })
+            .on('end', function () {
+
+                const stat = {
+                    "feets": calculate_average(average_feet),
+                    "heights": calculate_average(average_height)
+                }
+                stats.push(stat)
+                console.table(users)
+                console.table(stats)
+
+            })
+    } else {
+        console.error('File does not exist')
+    }
+}
+
 
 function generateUsername(firstname, surname) {
     return `${firstname[0]}-${surname}`.toLowerCase();
 }
+
+function addToHeightTab(height) {
+    average_height.push(height)
+    console.log(average_height)
+}
+
+function addToFeetTab(feet) {
+    average_feet.push(feet);
+    console.log(average_feet)
+}
+
+function calculate_average(tab) {
+    let avg = 0;
+    for (const value in tab) {
+        avg = Number(tab[value]) + avg;
+    }
+    return avg / tab.length;
+}
+
+function verifyFileExist(fileName) {
+    return fs.existsSync(fileName);
+}
+
+function verifyIsCsvFile(fileName){
+    return fileName.split(".")[1] === "csv";
+}
+
+main();
